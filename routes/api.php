ca03842d33af7a5b2a496b06d1817dced3627ebf
Paulin Caratini <p.caratini@outlook.fr>
2020-04-03 19:50:42 +0200

    Modifications routes

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', 'UserController@show');
-Route::middleware('auth:sanctum')->get('/user/logged', 'UserController@isUserLogged');
+Route::get('/user', 'UserController@show')->middleware('auth:sanctum');
+Route::get('/user/logged', 'UserController@isUserLogged');
